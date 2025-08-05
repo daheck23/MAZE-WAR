8ed55d236db5736aef81945f7aa177bcd7014242
Dave <dheck23@icloud.com>
2025-08-05 13:23:44 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from gui.start_dialog import StartDialog
 
+def main():
+    app = QApplication(sys.argv)
+    start_dialog = StartDialog()
+    start_dialog.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
